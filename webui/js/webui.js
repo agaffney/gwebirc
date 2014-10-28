d39226fc51667f40b924364155c0fc54a42a9f85
Andrew Gaffney <andrew@agaffney.org>
2014-10-28 07:44:08 -0700

    Add breadcrumbs and JS to update them dynamically

--- webui/js/webui.js
+++ webui/js/webui.js
@@ -3,3 +3,29 @@ function get_connections() {
 		alert("You are connected to host " + data[0]['host'] + " with nick " + data[0]['nick'])
 	});
 }
+
+function test_breadcrumbs() {
+	var random_items = ["apple","banana","car","donut"].sort(function() {
+		return (Math.round(Math.random())-0.5);
+	});
+	var data = [
+		{ "text": "Home", "url": "#" },
+	];
+	for (var x = 0; x < random_items.length; x++) {
+		var item = random_items[x];
+		data.push({ "text": item, "url": "#" });
+	}
+	update_breadcrumbs(data);
+}
+
+function update_breadcrumbs(items) {
+	var br = $('.breadcrumb')
+	br.empty()
+	var items_len = items.length;
+	for (var i = 0; i < items_len - 1; i++) {
+		var item = items[i];
+		br.append('<li><a href="' + item['url'] + '">' + item['text'] + '</a></li>')
+	}
+	// Handle last item differently, since it's the current page
+	br.append('<li class="active">' + items[items_len - 1]['text'])
+}
